059f84b3b5421a3e32296e7758b128ea663c29d7
JonasDeichelmann <Jonas.Deichelmann@me.com>
2017-11-30 17:45:29 -0800

    Adding Date Extension

---
+++ TB/Date.swift
@@ -0,0 +1,17 @@
+//
+//  Date.swift
+//  TB
+//
+//  Created by Jonas Deichelmann on 30.11.17.
+//  Copyright © 2017 JonasDeichelmann. All rights reserved.
+//
+
+import Foundation
+extension Date {
+    func add(minutes: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    }
+    func add(hours: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .hour, value: hours, to: self)!
+    }
+}
