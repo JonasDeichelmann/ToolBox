85756d0499e138372feeedbe86751d3e4d3f9c47
JonasDeichelmann <Jonas.Deichelmann@me.com>
2017-12-06 22:18:37 -0800

    Changing RunScript File

---
+++ TB/RunScript.swift
@@ -0,0 +1,16 @@
+//
+//  RunScript.swift
+//  TB
+//
+//  Created by Jonas Deichelmann on 06.12.17.
+//  Copyright © 2017 JonasDeichelmann. All rights reserved.
+//
+
+/*
+
+ buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+ buildNumber=$(($buildNumber + 1))
+ /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
+
+
+*/
---
+++ TB/creatingPDF.swift
@@ -0,0 +1,9 @@
+//
+//  creatingPDF.swift
+//  TB
+//
+//  Created by Jonas Deichelmann on 01.12.17.
+//  Copyright © 2017 JonasDeichelmann. All rights reserved.
+//
+
+import Foundation
